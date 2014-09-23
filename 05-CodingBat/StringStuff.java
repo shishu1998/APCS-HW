24126e8d4342a8842c05caaea26125246f5f76a4
shishu1998 <shishu_1998@yahoo.com>
2014-09-23 18:35:33 -0400

    hw upload

---
+++ 05-CodingBat/StringStuff.java
@@ -0,0 +1,18 @@
+public class StringStuff{
+    //problem one
+    public String nonStart(String a, String b) {
+	return a.substring(1) + b.substring(1);
+    }
+    //problem two
+    public String makeAbba(String a, String b) {
+	return a + b + b + a;
+    }
+    //problem three
+    public int diff21(int n) {
+	if (n > 21) {
+	    return (n - 21) * 2;
+	}
+	else {
+	    return 21 - n;}
+    }
+}
