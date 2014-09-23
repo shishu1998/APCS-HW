4b4c0784caf9492e5a181522f9f1b85d041fd576
shishu1998 <shishu_1998@yahoo.com>
2014-09-23 18:34:57 -0400

    hw upload

---
+++ 05-CodingBat/Driver.java
@@ -0,0 +1,18 @@
+public class Driver{
+    public static void main(String[] args){
+	//first variable
+	StringStuff ss1 = new StringStuff();
+	//second variable
+	StringStuff ss2 = new StringStuff();
+	//third variable
+	StringStuff ss3 = new StringStuff();
+
+	//test one
+	System.out.println(ss1.nonStart("hello","goodbye"));
+	//test two
+	System.out.println(ss2.makeAbba("racecar","DrowsySword"));
+	//test three
+	System.out.println(ss3.diff21(5));
+    }
+}
+
