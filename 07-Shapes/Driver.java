40bb1dfc8fdb87ff456881a95985f122214db8e4
Shi <shishu_1998@yahoo.com>
2014-10-02 08:41:48 -0400

    class

--- 07-Shapes/Driver.java
+++ 07-Shapes/Driver.java
@@ -4,6 +4,6 @@ public class Driver {
 	System.out.println(s.box(5,4));
 	System.out.println(s.box(4,5));
 	System.out.println(s.tri1(4));
-	System.out.println(s.tri2(4));
+	System.out.println(s.tri2(5));
     }
 }
