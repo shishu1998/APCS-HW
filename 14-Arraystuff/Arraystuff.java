55ad079616e3e5a92552038f47aff5dc63f4974a
Shi Shu <shishu_1998@yahoo.com>
2014-11-05 08:40:53 -0500

    Mode

--- 14-Arraystuff/Arraystuff.java
+++ 14-Arraystuff/Arraystuff.java
@@ -167,13 +167,24 @@ public class Arraystuff {
 	}
 	return result;
     }
-
+    public int mode(){
+	int freq = 0;
+	int pos = 0;
+	for(int counter = 0;counter < a.length; counter ++){
+	    if (freq(a[counter]) > freq){
+		freq = freq(a[counter]);
+		pos = counter;
+	    }
+	}
+	return a[pos];
+    }
     public static void main(String[] args) {
 	Arraystuff as = new Arraystuff();
 	System.out.println(as);
 	System.out.println(as.find(100));
 	System.out.println(as.maxVal());
 	System.out.println(as.freq(2));
+       	System.out.println(as.mode());
 	
     }
 }
