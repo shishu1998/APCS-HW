73e66e2377fcb4b3731451e8054488767c91af00
Shi Shu <shishu_1998@yahoo.com>
2014-11-12 08:40:54 -0500

    Arraylist

---
+++ 16-Arraylist/Sarray2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Sarray2{
+    public static void main(String[] args) {
+	ArrayList<Integer> al= new ArrayList<Integer>();
+	Random r = new Random();
+	for (int i = 0; i < 20; i++) {
+	    ai.add(r.nextInt(5));
+	}
+	
+    }
+}
