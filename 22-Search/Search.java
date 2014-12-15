454f2245e9b2d2e5d0fab0abd65c99f4d4816bdd
shishu1998 <shishu_1998@yahoo.com>
2014-12-14 21:03:39 -0500

    Binary searches do not work if you're trying to find 0 at index 0

--- 22-Search/Search.java
+++ 22-Search/Search.java
@@ -1,22 +1,85 @@
 import java.io.*;
 import java.util.*;
 public class Search{
-    private ArrayList<Integers> al;
-    al = new ArrayList<Integers>();
+    private int [] a;
     private Random r = new Random();
-    public Search(){
-	for(int i = 0; i < 10000; i ++){
-	    al.set(i, r.nextInt(10000));
+    public Search(int size){
+	a = new int [size];
+	for(int i = 0; i < size; i ++){
+	    a[i] = r.nextInt(100);
 	}
     }
-    public comparable lsearch(ArrayList<comparable> a, comparable b){
-	for(int i = 0; i < a.size(); i ++){
-	    if(a.get(i).equals(b)){
-		return b;
-	    }
-	    if(i == a.size()){
-		return null;
+    public void sort(){
+	 Arrays.sort(a);
+    }
+    public int getlength(){
+	return a.length - 1;
+    }
+    public int getstuff(int index){
+	return a[index];
+    }
+    public int[] getarray(){
+	return a;
+    }
+    public int lsearch(int[] a, int b){
+        int result = -1;
+	for(int i = 0; i < a.length - 1; i ++){
+	    if(a[i] == b){
+		result = b;
 	    }
 	}
+	return result;
+    }
+    public int bsearch(int[] a, int b){
+        int high = a.length - 1;
+        int low = 0;
+	int mid;
+	int result = -1;
+	while(high != low + 1){
+	    mid = (high + low)/2;
+	    if(a[mid] == b){
+		result = b;
+		break;
+	    }
+	    else if(a[mid] < b){
+		low = mid;
+	    }
+	    else{
+		high = mid;
+	    }
+	    
+	}
+	return result;
+    }
+    public int rbsearch(int low, int high, int find){
+	int mid = (low + high) / 2;
+	if(high == low + 1){
+	    return -1;
+	}
+	else {
+	    if(a[mid] < find){
+		return rbsearch(mid, high, find);
+	    }
+	    if(a[mid] > find){
+		return rbsearch(low,mid,find);
+	    }
+	    else{
+		return find;
+	    }
+	}
+    }
+    public static void main(String[] args){
+	Search a = new Search(100);
+	a.sort();
+	for(int i = 0; i < a.getlength(); i ++){
+	    System.out.print(a.getstuff(i) + " ");
+	}
+	System.out.println();
+	System.out.println(a.lsearch(a.getarray(),3));
+	System.out.println(a.bsearch(a.getarray(),3));
+	System.out.println(a.rbsearch(0,a.getarray().length - 1, 3));
+	
+	
     }
 }
+
